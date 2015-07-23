edf4f7e6cdd62fc395c0c906f95fc2991b4b8648
LJ <sujyao@gmail.com>
2015-07-24 10:42:16 +1200

    pwm hold mode trial

--- DE0_NANO/test1/select_mode.v
+++ DE0_NANO/test1/select_mode.v
@@ -1,10 +1,11 @@
 module  select_mode(
 		input wire clk,key0,key1,
 		output wire led0,led1,
-		output reg pwm);
+		output wire pwm);
 		
 reg  [1:0] ledr;
 reg  [1:0] keys;
+reg pwms;
 integer counter;
 		 
 always @(posedge clk ) begin
@@ -14,18 +15,18 @@ always @(posedge clk ) begin
 		ledr <= 2'b01;
 		if(counter == 625) begin
 			counter = 0;
-			pwm = 1'b0; 
+			pwms <= 1'b0; 
 		end
 	
 		else begin
 			counter = counter + 1;
-			pwm = pwm;
+			pwms <= pwms;
 		end
 			
 		if(counter >= 200)
-			pwm = 1'b1;
+			pwms <= 1'b1;
 		else
-			pwm =1'b0;	
+			pwms <=1'b0;	
 		
 	end
 	
@@ -33,18 +34,18 @@ always @(posedge clk ) begin
 		ledr <= 2'b10;
 		if(counter == 625) begin
 			counter = 0;
-			pwm = 1'b0; 
+			pwms <= 1'b0; 
 		end
 	
 		else begin
 			counter = counter + 1;
-			pwm = pwm;
+			pwms <= pwms;
 		end
 			
 		if(counter >= 400)
-			pwm = 1'b1;
+			pwms <= 1'b1;
 		else
-			pwm =1'b0;	
+			pwms <=1'b0;	
 				
 		
 		
@@ -57,7 +58,8 @@ always @(posedge clk ) begin
 end
 
 assign led0 = ledr[0];
-assign led1 = ledr[1];		 
+assign led1 = ledr[1];	
+assign pwm = pwms;	 
 
 	
 endmodule 
